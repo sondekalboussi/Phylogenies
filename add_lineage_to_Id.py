543ca3ff826c9deba0ae96dc57d2e1a514bc590d
sondes <sondekalboussi@users.noreply.github.com>
2017-04-04 11:41:15 +0200

    Update add_lineage_to_Id.py

--- add_lineage_to_Id.py
+++ add_lineage_to_Id.py
@@ -1,27 +1,27 @@
 import os
 import fileinput
 import sys
-
-#Read the csv file, parse the headers
 def add_lineage(path,path1):
+    #Read the csv file, parse the headers
     f=open(path,"r").read().split('\n')
     F=f[1:]
     Exitref_Id={}
     for item in F:
-        Id=item and item.split(',')[]#add index of sample_id column
-        lineage=item and item.split(',')[]#add index of lineage column 
+        Id=item and item.split(',')[0]#IndexError problem
+        lineage=item and item.split(',')[]#add column index to lineage
         ID=Id.replace('-','')
-        QC=item and item.split(',')[]#add index of QC column  
+        QC=item and item.split(',')[]#add column index to QC
         if QC!='FAIL' and Id!='':
-                Exitref_Id[ID]=lineage
+            Exitref_Id[]=lineage # if the sample name in the tree/fasta contains a - use Exitref_Id[Id] otherwise use Exitref_Id[ID]
     for key,value in Exitref_Id.iteritems():
         for lines in fileinput.input(path1,inplace=True):
             if key in lines:
                 lines=lines.replace(key,key+'-'+value)
             sys.stdout.write(lines)
-    
             
 path= #path to csv file with lineages and samples id
 path1=#path to fasta file or tree file     
 Final=add_lineage(path,path1)
 print Final
+
+
